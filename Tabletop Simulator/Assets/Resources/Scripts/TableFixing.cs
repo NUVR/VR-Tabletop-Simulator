958451805b30fad34fecdad838c794844fe26930
Peter Abbondanzo <peter@abbondanzo.com>
2018-10-23 19:46:53 -0400

    Reorganizing assets

---
+++ Tabletop Simulator/Assets/Resources/Scripts/TableFixing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableFixing : MonoBehaviour {
+
+    [SerializeField]
+    protected Collider[] collisionDetectors = null;
+
+    private Collider collisionRegion;
+
+    // Use this for initialization
+    void Start () {
+        this.collisionRegion = GameObject.GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Test");
+	}
+}
