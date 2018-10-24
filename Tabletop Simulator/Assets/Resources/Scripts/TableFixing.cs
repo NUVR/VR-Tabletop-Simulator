afa36ea59c95ff1f8f00c7f40258087dfaf81453
Peter Abbondanzo <peter@abbondanzo.com>
2018-10-23 20:25:41 -0400

    Update Tabletop Simulator/Assets/Resources/Scripts/TableFixing.cs

--- Tabletop Simulator/Assets/Resources/Scripts/TableFixing.cs
+++ Tabletop Simulator/Assets/Resources/Scripts/TableFixing.cs
@@ -7,15 +7,37 @@ public class TableFixing : MonoBehaviour {
     [SerializeField]
     protected Collider[] collisionDetectors = null;
 
+    private bool isPressing;
     private Collider collisionRegion;
 
     // Use this for initialization
     void Start () {
-        this.collisionRegion = GameObject.GetComponent<Collider>();
+        this.collisionRegion = gameObject.GetComponent<Collider>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log("Test");
-	}
+        DetectTrigger();
+    }
+
+    private void DetectTrigger()
+    {
+        float hand = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch);
+        float index = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch);
+        bool isPressing = hand + index > 1;
+        if (this.isPressing != isPressing)
+        {
+            this.isPressing = isPressing;
+            Debug.Log(isPressing ? "Pressing" : "Released");
+        }
+        if (hand + index > 1)
+        {
+            
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("COLLISION");
+    }
 }
