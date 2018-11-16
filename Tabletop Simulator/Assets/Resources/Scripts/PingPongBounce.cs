41d86ca3dba10e487906873f4680582b47c81197
Peter Abbondanzo <peter@abbondanzo.com>
2018-11-15 21:02:04 -0500

    Rip

---
+++ Tabletop Simulator/Assets/Resources/Scripts/PingPongBounce.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongBounce : MonoBehaviour {
+    public Rigidbody ball;
+    // Use this for initialization
+    void Start () {
+        ball = GetComponent<Rigidbody>();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var direction = Vector3.Reflect(ball.position, collision.contacts[0].point);
+        ball.velocity = direction * 5;
+        //foreach (ContactPoint contact in collision.contacts)
+        //{
+        //   print(contact.point);
+        //}
+        //Vector3 ballPosition = collision.contacts[0].point;
+        //Vector3 paddlePosition = collision.contacts[1].point;
+        //Vector3 shootDir = ballPosition - paddlePosition; // Calculate direction of the shot
+        //ball.AddForce(Camera.main.transform.forward * 5, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
