4d3d3d0f4df6fe6aa71646ed2a33318c2880a600
Peter Abbondanzo <peter@abbondanzo.com>
2019-04-11 20:48:48 -0400

    Add blur manager

---
+++ Tabletop Simulator/Assets/Resources/Scripts/BlurManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SuperBlur.SuperBlurBase))]
+public class BlurManager : MonoBehaviour
+{
+
+    private SuperBlur.SuperBlurBase blur;
+    // Start is called before the first frame update
+    void Start()
+    {
+        blur = GetComponent<SuperBlur.SuperBlurBase>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        blur.interpolation += 0.0001f;
+    }
+}
